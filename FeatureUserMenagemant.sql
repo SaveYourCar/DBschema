fbdda56a88ba9c9cf3b836a4b2055421fa79554e
filippo basciu <basciu45@gmail.com>
2019-10-24 10:26:53 +0200

    UPDATE upper cases

--- FeatureUserMenagemant.sql
+++ FeatureUserMenagemant.sql
@@ -1,46 +1,52 @@
--- TABLE
-CREATE TABLE SaveYourCar_Users_AnagraphicTable (
-  PersonID int PRIMARY KEY,
-  LastName varchar(255) NOT NULL,
-  FirstName varchar(255)NOT NULL,
-  Country varchar(255),
+CREATE TABLE SaveYourCar_Users_Anagraphic(   
+  
+  id INT PRIMARY KEY,
+  LastName VARCHAR(255) NOT NULL,
+  FirstName VARCHAR(255)NOT NULL,
+  Country VARCHAR(255),
   Region VARCHAR(255),
-  City varchar(255),
+  City VARCHAR(255),
   Email VARCHAR(255) NOT NULL
-);
-CREATE TABLE SaveYourCar_Users_GroupsAdministrators(
+  
+  );
+
+CREATE TABLE SaveYourCar_Users_GroupsAdministrators(   
   id INT PRIMARY KEY,
   AdminID INT,
   Groups INT,
   Description VARCHAR 
+  
   );
-CREATE TABLE SaveYourCar_Users_GroupsSubdivision(
+
+CREATE TABLE SaveYourCar_Users_GroupsSubdivision(   
   id INT PRIMARY KEY,
   UserID int,
   Groups INT
+  
   );
-CREATE TABLE SaveYourCar_Users_LevelsTable (
-  ID int PRIMARY KEY,
+
+CREATE TABLE SaveYourCar_Users_Levels(   
+
+  id INT PRIMARY KEY,
   UserID  INT,
   Level INT NOT NULL
-);
-CREATE TABLE SaveYourCar_Users_LoginTable(
+
+  );
+
+CREATE TABLE SaveYourCar_Users_Login(     
+
   id INT PRIMARY KEY,
   UserID Int, 
-  UserName Varchar (255)NOT NULL,
+  UserName VARCHAR (255)NOT NULL,
   Password VARCHAR (255)NOT NULL,
   LevelID INT 
+  
   );
-CREATE TABLE SaveYourCar_Users_MechanicChoices(
+
+CREATE TABLE SaveYourCar_Users_MechanicChoices(     
   id INT PRIMARY KEY,
-  UserID int,
+  UserID INT,
   MechanicID INT,
   Rate INT
+  
   );
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
