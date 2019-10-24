709c3e1f1001b80224ad3138eecc8e8a476fe78d
Acrobat <lucagsls@gmail.com>
2019-10-24 12:03:44 +0200

    UPDATE upper cases; ADD comments

--- FeatureUserMenagemant.sql
+++ FeatureUserMenagemant.sql
@@ -1,4 +1,60 @@
+
 CREATE TABLE SaveYourCar_Users_Anagraphic(   
+  
+  id INT PRIMARY KEY,
+  LastName VARCHAR(255) NOT NULL,
+  FirstName VARCHAR(255)NOT NULL,
+  Country VARCHAR(255),
+  Region VARCHAR(255),
+  City VARCHAR(255),
+  Email VARCHAR(255) NOT NULL
+  
+  );
+
+CREATE TABLE SaveYourCar_Users_GroupsAdministrators(   -- creation table of groups  managed by administrators
+ 
+  id INT PRIMARY KEY,
+  AdminID INT,
+  Groups INT,
+  Description VARCHAR 
+  
+  );
+
+CREATE TABLE SaveYourCar_Users_GroupsSubdivision(    -- table of suddivision users by groups
+  id INT PRIMARY KEY,
+  UserID INT,
+  Groups INT
+  
+  );
+
+CREATE TABLE SaveYourCar_Users_Levels(   
+
+  id INT PRIMARY KEY,
+  UserID  INT,
+  Level INT NOT NULL
+
+  );
+
+CREATE TABLE SaveYourCar_Users_Login(     
+
+  id INT PRIMARY KEY,
+  UserID INT, 
+  UserName VARCHAR (255)NOT NULL,
+  Password VARCHAR (255)NOT NULL,
+  LevelID INT 
+  
+  );
+
+CREATE TABLE SaveYourCar_Users_MechanicChoices(     -- relations table between users and mechanics choice
+
+  id INT PRIMARY KEY,
+  UserID INT,
+  MechanicID INT,
+  Rate INT
+  
+  );
+ 
+ CREATE TABLE SaveYourCar_Users_Anagraphic(   
   
   id INT PRIMARY KEY,
   LastName VARCHAR(255) NOT NULL,
@@ -20,7 +76,7 @@ CREATE TABLE SaveYourCar_Users_GroupsAdministrators(
 
 CREATE TABLE SaveYourCar_Users_GroupsSubdivision(   
   id INT PRIMARY KEY,
-  UserID int,
+  UserID INT,
   Groups INT
   
   );
@@ -36,7 +92,7 @@ CREATE TABLE SaveYourCar_Users_Levels(
 CREATE TABLE SaveYourCar_Users_Login(     
 
   id INT PRIMARY KEY,
-  UserID Int, 
+  UserID INT, 
   UserName VARCHAR (255)NOT NULL,
   Password VARCHAR (255)NOT NULL,
   LevelID INT 
