3ffc26193ba05039e1f0fcc19ca60cbbeffeee30
Ivana <ivana.zaccheddu88@gmail.com>
2019-10-25 10:28:21 +0200

    CREATE Feature Car Menagemant Tables

---
+++ FeatureCarMenagemant.sql
@@ -0,0 +1,35 @@
+CREATE TABLE SaveYourCar_Cars_Anagraphic (  
+id INT PRIMARY KEY,
+CarName VARCHAR(255) NOT NULL,
+Fuel VARCHAR(255) NOT NULL,
+MatriculatinDate DATE
+);
+
+
+CREATE TABLE SaveYourCar_Cars_Data (       
+id INT PRIMARY KEY,
+DATA VARCHAR NOT NULL,
+TYPE VARCHAR NOT NULL
+);
+
+
+
+CREATE TABLE SaveYourCar_Cars_RelationDataCar(   
+  
+  id INT PRIMARY KEY,
+  CarId INT NOT NULL,
+  ValueInt INT,                                                --  One field between ValueInt and ValueDate has to be  NULL
+  ValueDate DATE,
+  DetectionDate DATE,
+  DetectorName VARCHAR(255)
+  
+  );
+
+
+CREATE TABLE SaveYourCar_Cars_RelationCarsUsers(   
+
+  id INT PRIMARY KEY,
+  UserID  INT,
+  CarId INT
+
+  );
