24be71a82d5def5a37a88937a6f50ba674f592d1
Ivana <ivana.zaccheddu88@gmail.com>
2019-11-10 10:07:17 +0100

    UPDATE INT type to INTEGER; CHANGE DataType type on Cars_Data; UPDATE id_names

--- FeatureCarMenagemant.sql
+++ FeatureCarMenagemant.sql
@@ -1,5 +1,5 @@
 CREATE TABLE SaveYourCar_Cars_Anagraphic (  
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   CarName VARCHAR(255) NOT NULL,
   Fuel VARCHAR(255) NOT NULL,
   MatriculatinDate DATE
@@ -7,16 +7,16 @@ CREATE TABLE SaveYourCar_Cars_Anagraphic (
 
 
 CREATE TABLE SaveYourCar_Cars_Data (       
-  id INT PRIMARY KEY,
-  CarData VARCHAR NOT NULL,
-  DataType VARCHAR NOT NULL
+  id INTEGER PRIMARY KEY,
+  CarData VARCHAR(255) NOT NULL,
+  DataType BOOLEAN NOT NULL
   );
 
 
 CREATE TABLE SaveYourCar_Cars_RelationDataCar(   
-  id INT PRIMARY KEY,
-  CarId INT NOT NULL,
-  ValueInt INT,                                                --  One field between ValueInt and ValueDate has to be  NULL
+  id INTEGER PRIMARY KEY,
+  id_Car INTEGER NOT NULL,
+  ValueInt INTEGER,                                                --  One field between ValueInt and ValueDate has to be  NULL
   ValueDate DATE,
   DetectionDate DATE,
   DetectorName VARCHAR(255)
@@ -24,7 +24,7 @@ CREATE TABLE SaveYourCar_Cars_RelationDataCar(
 
 
 CREATE TABLE SaveYourCar_Cars_RelationCarsUsers(   
-  id INT PRIMARY KEY,
-  UserId  INT,
-  CarId INT
+  id INTEGER PRIMARY KEY,
+  id_User  INTEGER,
+  id_Car INTEGER
   );
